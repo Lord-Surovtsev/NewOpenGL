fc21dead8c65df426140d7248415614f226a34c2
lord-surovtsev <lord.surovtsev@gmail.com>
2014-05-12 11:31:21 +0400

    next iteration

--- NewOpenGL.pro
+++ NewOpenGL.pro
@@ -1,9 +1,16 @@
-TEMPLATE = app
-CONFIG += console
-CONFIG -= app_bundle
-CONFIG -= qt
+QT += core #gui opengl
+#TEMPLATE = app
+#CONFIG += console
+#CONFIG -= app_bundle
+#CONFIG -= qt
 
-LIBS += libfreeglut -lopengl32 -lglu32 -luser32 -lsfml-system
+#LIBS += libfreeglut -lopengl32 -lglu32 -luser32 -lsfml-system
+#INCLUDEPATH += /usr/include
+
+#LIBS += -lSDL2 -lGL -lGLU -lgl3w -lX11
+#LIBS += -lGLEW #-lglut -lGL -lGLU
+LIBS += -lglut -lGL -lGLU -lGLEW
+LIBS += -lsfml-system
 
 SOURCES += \
     main.cpp
--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
+#define GLX_GLXEXT_PROTOTYPES
+//#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GL/glx.h>
+
+//#ifdef LINUX
+//#define GLX_GLXEXT_PROTOTYPES
+//#include <GL/glx.h>
+//#define wglxGetProcAddress(a) glXGetProcAddressARB((const GLubyte *) (a))
+//#endif
+
 #include <iostream>
 using namespace std;
 
 #include <GL/glut.h>
-
 #include <SFML/System.hpp>
+
+//extern void (*glXGetProcAddressARB(const GLubyte *))();
 
 int main(int argc, char* argv[])
 {
+    // Specify prototype of function
+    typedef void (*GENBUFFERS) (GLsizei, GLuint*);
+
+    //// Load address of function and assign it to a function pointer
+    //GENBUFFERS glGenBuffers = (GENBUFFERS)wglGetProcAddress("glGenBuffers");
+    // or Linux:
+    GENBUFFERS glGenBuffers = (GENBUFFERS)glXGetProcAddress((const GLubyte *) "glGenBuffers");
+    //// or OSX:
+    //GENBUFFERS glGenBuffers = (GENBUFFERS)NSGLGetProcAddress("glGenBuffers");
+
+    // Call function as normal
+    GLuint buffer;
+    glGenBuffers(1, &buffer);
+
+
     cout << "aaa" << endl;
     sf::sleep(sf::seconds(1.f));
     cout << "bbb" << endl;
